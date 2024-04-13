eb63f5039e759d48f1d478c0688045185a70b875
David Sass <elethulyelenseg@hotmail.com>
2024-04-13 21:17:24 +0200

    fix(typo)

--- dist/index.js
+++ dist/index.js
@@ -1347,32 +1347,32 @@ function main() {
             yield PowerShellToolRunner_1.default.init();
             const script = `$ErrorActionPreference = "Stop";
             $WarningPreference = "SilentlyContinue";
-
+            
             $sharingCapabilityDisabledSites = if ( 'null' -ne '${sharingCapabilityDisabled}' ) { '${sharingCapabilityDisabled}'.Split(",").Trim().TrimEnd('/').ToLower() } else { @() };
             "To Disable site count: $($sharingCapabilityDisabledSites.count)" | Write-Output;
-
+            
             $sharingCapabilityExternalUserSharingOnlySites = if ( 'null' -ne '${sharingCapabilityExternalUserSharingOnly}' ) { '${sharingCapabilityExternalUserSharingOnly}'.Split(",").Trim().TrimEnd('/').ToLower() } else { @() };
             "To ExternalUserSharingOnly site count: $($sharingCapabilityExternalUserSharingOnlySites.count)" | Write-Output;
-
+            
             $sharingCapabilityExternalUserAndGuestSharingSites = if ( 'null' -ne '${sharingCapabilityExternalUserAndGuestSharing}' ) { '${sharingCapabilityExternalUserAndGuestSharing}'.Split(",").Trim().TrimEnd('/').ToLower() } else { @() };
             "To ExternalAndGuestSharing site count: $($sharingCapabilityExternalUserAndGuestSharingSites.count)" | Write-Output;
-
+            
             $sharingCapabilityExistingExternalUserSharingOnlySites = if ( 'null' -ne '${sharingCapabilityExistingExternalUserSharingOnly}' ) { '${sharingCapabilityExistingExternalUserSharingOnly}'.Split(",").Trim().TrimEnd('/').ToLower() } else { @() };
             "To ExistingExternalUserSharingOnly site count: $($sharingCapabilityExistingExternalUserSharingOnlySites.count)" | Write-Output;
-
-            $sharingOrderOfPrecedence = new-object 'System.Collections.Specialized.OrderedDictionary';
+            
+            $sharingOrderOfPrecedence = New-Object 'System.Collections.Specialized.OrderedDictionary';
             $sharingOrderOfPrecedence.Add('ExternalUserAndGuestSharing', $sharingCapabilityExternalUserAndGuestSharingSites);
             $sharingOrderOfPrecedence.Add('ExternalUserSharingOnly', $sharingCapabilityExternalUserSharingOnlySites);
             $sharingOrderOfPrecedence.Add('ExistingExternalUserSharingOnly', $sharingCapabilityExistingExternalUserSharingOnlySites);
             $sharingOrderOfPrecedence.Add('Disabled', $sharingCapabilityDisabledSites);
-
-            foreach($sites in ($sharingOrderOfPrecedence.Values.GetEnumerator())){
+            
+            foreach ($sites in ($sharingOrderOfPrecedence.Values.GetEnumerator())) {
                 $sites.count | Write-Output;
             }
-
+            
             # Now lets check that only one parameter contains 'ALLELSE', and make sure that one will get executed last.
             $allCount = 0
-
+            
             if ($sharingCapabilityDisabledSites -icontains 'ALLELSE') {
                 $allCount++; $allElse = 'Disabled';
                 $sharingOrderOfPrecedence.Remove('Disabled')
@@ -1389,28 +1389,29 @@ function main() {
                 $allCount++; $allElse = 'ExistingExternalUserSharingOnly';
                 $sharingOrderOfPrecedence.Remove('ExistingExternalUserSharingOnly')
             }
-
+            
             if ($allCount -le 1) {
                 Write-Output "✅ Only one parameter contains 'ALLELSE'";
                 Write-Output "🚩 All else: $allElse";
-            } else {
+            }
+            else {
                 Throw "More than one parameter contains 'ALLELSE' this is not allowed, make sure that maximum only one parameter contains 'ALLELSE'"
             }
-
-            Install-Module -Name SharePointPnPPowerShellOnline  -Force -Verbose -Scope CurrentUser;
+            
+            Install-Module -Name SharePointPnPPowerShellOnline -Force -Verbose -Scope CurrentUser;
             Connect-PnPOnline -Url ${adminUrl} -ClientId ${clientID} -ClientSecret ${clientSecret};
-            Get-PnPTenantSite | ft Url, Template, LocaleId, SharingCapability | Write-Output;
-
+            Get-PnPTenantSite | Format-Table Url, Template, LocaleId, SharingCapability | Write-Output;
+            
             $sitesAsSet = New-Object System.Collections.Generic.HashSet[string]
-            Get-PnPTenantSite | foreach-object{ $null = $sitesAsSet.add($_.url.ToLower().TrimEnd('/'))) }
+            Get-PnPTenantSite | ForEach-Object { $null = $sitesAsSet.add( $_.url.ToLower().TrimEnd('/') ) }
             Write-Output "All sites count: $($sitesAsSet.count)";
-
+            
             Write-Output "🚀 Start to update the sharing capability for the sites";
-
+            
             foreach ($sharingCapability in $sharingOrderOfPrecedence.GetEnumerator()) {
                 $sharingCapabilityName = $sharingCapability.Key;
                 $sites = $sharingCapability.Value;
-                if ($sites -ne $null -and $sites.Count -gt 0) {
+                if ($null -ne $sites -and $sites.Count -gt 0) {
                     Write-Output "🚀 Start to update the sharing capability for the sites with sharing capability: $sharingCapabilityName";
                     foreach ($site in $sites) {
                         Write-Output "🚀🚀 Start to update the sharing capability for the site: $site";
@@ -1420,20 +1421,20 @@ function main() {
                     }
                 }
             }
-
+            
             if ($allCount -eq 1) {
                 # Get All sites, and filter out the sites that are already updated.
-                Write-output "Remaining sites to update: $($sitesAsSet.count)";
+                Write-Output "Remaining sites to update: $($sitesAsSet.count)";
                 foreach ($site in $sitesAsSet) {
                     Write-Output "🚀🚀 Start to update the sharing capability for the site: $site";
                     Set-PnPTenantSite -Url $site -SharingCapability $allElse -ErrorAction Continue;
                     Write-Output "✅ Sharing capability updated for the site: $site";
                 }
             }
-
+            
             Write-Output "✅ All sites are updated successfully";
-            Get-PnPTenantSite | ft Url, Template, LocaleId, SharingCapability | Write-Output;
-            `;
+            Get-PnPTenantSite | Format-Table Url, Template, LocaleId, SharingCapability | Write-Output;
+                        `;
 
             yield PowerShellToolRunner_1.default.executePowerShellScriptBlock(script);
             core.info("✅ Something is successful.");
